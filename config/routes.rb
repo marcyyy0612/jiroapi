f944f2195bfe232eda1311bdf4dedd713849b4b5
marcyyy0612 <marcyyy0612@gmail.com>
2018-06-09 14:26:04 +0900

    cannot post users

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   resources :foods
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/search/genres', to: 'foods#search_genre'
+
+  devise_for :user, only: []
+
+  namespace :v1, defaults: { format: :json } do
+    resource :login, only: [:create], controller: :sessions
+    resource :users, only: [:create]
+  end
 end
